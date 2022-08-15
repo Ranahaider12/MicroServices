4b626536fcede8bf4c9319e3519f92c59582e6db
Haider Ali <workhaider0@gmail.com>
2022-08-16 00:02:44 +0500

    db

---
+++ MicroServicesProject/UserMicroservices/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroservices.Database.Entities;
+
+namespace UserMicroservices.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=LHR-TCH-HAIDERA;Initial Catalog=MicroDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ MicroServicesProject/UserMicroservices/Database/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserMicroservices.Database.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string Status { get; set; }
+    }
+}
